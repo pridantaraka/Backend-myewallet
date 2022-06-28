68de36d27fab16e607b6b0c4d4627691ad05fb72
pridantaraka <pridantaraka@gmail.com>
2022-06-28 16:19:57 +0700

    tambahan tampilan pada index yang bisa di panggil di postman

--- index.js
+++ index.js
@@ -2,6 +2,22 @@ const express = require('express')
 
 const app = express()
 
-app.use(express,urlencoded({extends: false}))
+app.use(express.urlencoded({extends: false}))
 
-app.get('/')
+app.get('/',(req, res)=>{
+    return res.json({
+        success: true,
+        message: 'is running well'
+    })
+})
+
+app.post('/',(req, res)=>{
+    return res.json({
+        success: true,
+        message: 'posted data success to send'
+    })
+})
+
+app.listen(3333, ()=>{
+    console.log('app is running on port 3333');
+})
