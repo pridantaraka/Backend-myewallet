d08c38b831b1da1baef53c9d63c7372c3bfc8780
pridantaraka <pridantaraka@gmail.com>
2022-09-18 16:37:30 +0700

    update

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ app.get('/',(req, res)=>{
 // app.get('/', function (req, res) {
 //     res.json({msg: 'This is CORS-enabled for all origins!'});
 // });
- 
 // app.listen(80, function () {
 //     console.log('CORS-enabled web server listening on port 80');
 // });
