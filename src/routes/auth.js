4c4432133a57639b150c6992ac6ec0f7cd19a51c
pridantaraka <pridantaraka@gmail.com>
2022-10-27 10:53:48 +0700

    update patch pin

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const rules = require('./authValidator');
 const validation = require('../middleware/validation');
 
 auth.post('/register', ...rules.register, validation, authController.register);
-auth.post('/pin', ...rules.pin, validation, authController.createPin);
+auth.patch('/pin', ...rules.pin, validation, authController.createPin);
 auth.post('/login', ...rules.login, validation, authController.login);
 
 module.exports = auth;
